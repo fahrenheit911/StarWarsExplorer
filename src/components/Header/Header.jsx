71c53a6c3b9d9b9ec775422f87c2f3cb85f6b22d
OlegGolovko <oleg.golovko.job@gmail.com>
2024-05-05 15:19:22 +0200

    added active className for page's navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+
 import './header.css';
 
 export const Header = () => {
+  const getClassName = obj => {
+    let className = 'menu__link';
+    if (obj.isActive) className += '__active';
+    return className;
+  };
+
   return (
     <header className="header">
       <nav className="menu">
         <ul className="menu__list">
           <li className="menu__list-item">
-            <NavLink className="menu__link" to="/people">
+            <NavLink className={getClassName} to="/people">
               People
             </NavLink>
           </li>
           <li className="menu__list-item">
-            <NavLink className="menu__link" to="/planets">
+            <NavLink className={getClassName} to="/planets">
               Planets
             </NavLink>
           </li>
           <li className="menu__list-item">
-            <NavLink className="menu__link" to="/starships">
+            <NavLink className={getClassName} to="/starships">
               Starships
             </NavLink>
           </li>
