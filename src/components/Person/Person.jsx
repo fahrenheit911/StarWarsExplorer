14856b02fb5c971b575e33d7f3f136bcbea4f59d
OlegGolovko <oleg.golovko.job@gmail.com>
2024-05-06 23:22:09 +0200

    added funtion to create initials for avatar

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import './person.css';
 
 export const Person = ({index, name, birth_year, gender, clickPerson}) => {
+  const getInitials = name => {
+    const words = name.split(/[\s-]+/);
+    let initials = '';
+    for (let i = 0; i < words.length; i++) {
+      initials += words[i].charAt(0).toUpperCase();
+    }
+    return initials;
+  };
+  const initials = getInitials(name);
+
   return (
     <section className="person">
       <div className="person__card" onClick={() => clickPerson(name, index)}>
-        <div className="person__circle"></div>
+        <div className="person__circle">
+          <div className="person__initials">{initials}</div>
+        </div>
         <div className="person__data">
           <div>Name: {name}</div>
           <div>Birth year: {birth_year}</div>
