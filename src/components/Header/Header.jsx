3ac43c83d137d5d6dd2af0732fa1ed7e3238f68b
OlegGolovko <oleg.golovko.job@gmail.com>
2024-05-08 13:42:47 +0200

    created active condition to navigation in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import classNames from 'classnames';
+import {NavLink, useLocation} from 'react-router-dom';
 
 import './header.css';
 
 export const Header = () => {
-  const getClassName = obj => {
-    let className = 'menu__link';
-    if (obj.isActive) className += '__active';
-    return className;
-  };
+  const location = useLocation();
 
   return (
     <header className="header">
       <nav className="menu">
         <ul className="menu__list">
           <li className="menu__list-item">
-            <NavLink className={getClassName} to="/people">
+            <NavLink
+              className={classNames('menu__link', {
+                menu__link__active: location.pathname === '/people',
+              })}
+              to="/people"
+            >
               People
             </NavLink>
           </li>
           <li className="menu__list-item">
-            <NavLink className={getClassName} to="/planets">
+            <NavLink
+              className={classNames('menu__link', {
+                menu__link__active: location.pathname === '/planets',
+              })}
+              to="/planets"
+            >
               Planets
             </NavLink>
           </li>
           <li className="menu__list-item">
-            <NavLink className={getClassName} to="/starships">
+            <NavLink
+              className={classNames('menu__link', {
+                menu__link__active: location.pathname === '/starships',
+              })}
+              to="/starships"
+            >
               Starships
             </NavLink>
           </li>
