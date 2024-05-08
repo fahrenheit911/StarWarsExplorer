6d5c565c4a5f4f97abfa01d50c43a3a4b89fb8a3
OlegGolovko <oleg.golovko.job@gmail.com>
2024-05-08 17:08:13 +0200

    created a fetch function to download vehicles and starships data

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -8,6 +8,8 @@ import './modalwindow.css';
 export const ModalWindow = ({onClose}) => {
   const [homeworldData, setHomeworldData] = useState([]);
   const [filmsData, setFilmsData] = useState([]);
+  const [vehiclesData, setVehiclesData] = useState([]);
+  const [starshipsData, setStarshipsData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const person = useSelector(state => state?.person?.data);
@@ -29,7 +31,7 @@ export const ModalWindow = ({onClose}) => {
       }
     };
     fetchHomeworldName();
-  }, []);
+  }, [person.homeworld]);
 
   useEffect(() => {
     const fetchFilmsData = async () => {
@@ -50,7 +52,49 @@ export const ModalWindow = ({onClose}) => {
       setFilmsData(films);
     };
     fetchFilmsData();
-  }, []);
+  }, [person.films]);
+
+  useEffect(() => {
+    const fetchVehiclesData = async () => {
+      const vehicles = [];
+      for (const url of person.vehicles) {
+        try {
+          const response = await fetch(url);
+          if (response.ok) {
+            const vehicleData = await response.json();
+            vehicles.push(vehicleData);
+          } else {
+            throw new Error('Failed to fetch homeworld data');
+          }
+        } catch (error) {
+          console.error('Error fetching vehicle data:', error);
+        }
+      }
+      setVehiclesData(vehicles);
+    };
+    fetchVehiclesData();
+  }, [person.vehicles]);
+
+  useEffect(() => {
+    const fetchStarshipsData = async () => {
+      const starships = [];
+      for (const url of person.starships) {
+        try {
+          const response = await fetch(url);
+          if (response.ok) {
+            const starshipData = await response.json();
+            starships.push(starshipData);
+          } else {
+            throw new Error('Failed to fetch homeworld data');
+          }
+        } catch (error) {
+          console.error('Error fetching vehicle data:', error);
+        }
+      }
+      setStarshipsData(starships);
+    };
+    fetchStarshipsData();
+  }, [person.starships]);
 
   const onWrapperClick = e => {
     if (e.target.classList.contains('modal__wrapper')) onClose();
@@ -107,10 +151,10 @@ export const ModalWindow = ({onClose}) => {
                   <div className="block__vehicles">
                     <div>Vehicles:</div>
                     <ul>
-                      {person.vehicles.map((vehicle, index) => (
+                      {vehiclesData.map((vehicle, index) => (
                         <li key={index}>
-                          <NavLink className="link" to={vehicle}>
-                            {vehicle}
+                          <NavLink className="link" to={vehicle.url}>
+                            {vehicle.name}
                           </NavLink>
                         </li>
                       ))}
@@ -119,10 +163,10 @@ export const ModalWindow = ({onClose}) => {
                   <div className="block__starships">
                     <div>Starships: </div>
                     <ul>
-                      {person.starships.map((starship, index) => (
+                      {starshipsData.map((starship, index) => (
                         <li key={index}>
-                          <NavLink className="link" to={starship}>
-                            {starship}
+                          <NavLink className="link" to={starship.url}>
+                            {starship.name}
                           </NavLink>
                         </li>
                       ))}
