c88d6a756cc5e3fb2d002d3930b4deae5b63789d
OlegGolovko <oleg.golovko.job@gmail.com>
2024-05-14 11:54:39 +0200

    created Planets and Planet components

--- src/pages/Planets.jsx
+++ src/pages/Planets.jsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useParams} from 'react-router-dom';
 import Planet from '../components/Planet';
 import Button from '../components/Button';
+import ModalWindow from '../components/ModalWindow';
+import {loadData, nextLoadData, getPersonData} from '../Utils/dataLoad';
+import {
+  updateLoadStatePlanets,
+  updateDataPlanets,
+  updateNewDataPlanets,
+} from '../redux/planetsSlice.js';
+
+import './page.css';
 
 export const Planets = () => {
-  const loadMore = () => {
-    console.log('Planets button clicked!');
-  };
+  const [planetUrlId, setPlanetUrlId] = useState(null);
+
+  console.log(planetUrlId);
+
+  const params = useParams();
+  const dispatch = useDispatch();
+
+  const planets = useSelector(state => state?.planets?.data?.results);
+  const loading = useSelector(state => state?.planets?.isLoading);
+  const nextUrl = useSelector(state => state?.planets?.data?.next);
+
+  useEffect(() => {
+    setPlanetUrlId(params.plid);
+    if (planetUrlId) dispatch(getPersonData(planetUrlId));
+  }, [dispatch, params.plid, planetUrlId]);
+
+  useEffect(() => {
+    dispatch(
+      loadData('https://swapi.py4e.com/api/planets', updateLoadStatePlanets, updateDataPlanets)
+    );
+  }, [dispatch]);
+
   return (
     <article>
-      <section>
-        <Planet />
+      <section className="container">
+        {planets.map((planet, index) => (
+          <Planet key={index} index={index} {...planet} />
+        ))}
       </section>
-      <Button title="Load more" onClick={loadMore} />
+      <Button title={loading ? 'Loading...' : 'Load more'} disabled={loading} />
+      {planetUrlId && <ModalWindow />}
     </article>
   );
 };
