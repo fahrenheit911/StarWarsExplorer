d48b7a2a89080853e176ba952845f0182723ae5f
OlegGolovko <oleg.golovko.job@gmail.com>
2025-08-05 14:35:20 +0200

    Remove HashRouter wrapper from App component, simplifying routing structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import {HashRouter, BrowserRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import Header from '../Header';
 import {PagesRouter} from '../../routes/PagesRouter';
 
 export const App = () => {
   return (
-    <HashRouter>
-      <BrowserRouter basename="/StarWarsExplorer">
-        <main>
-          <Header />
-          <PagesRouter />
-        </main>
-      </BrowserRouter>
-    </HashRouter>
+    <BrowserRouter basename="/StarWarsExplorer">
+      <main>
+        <Header />
+        <PagesRouter />
+      </main>
+    </BrowserRouter>
   );
 };
