b15780f1c15f4d15804e8df4f8e2f905597ef603
OlegGolovko <oleg.golovko.job@gmail.com>
2025-08-05 14:20:21 +0200

    Wrap BrowserRouter in HashRouter for improved routing in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter, BrowserRouter} from 'react-router-dom';
 import Header from '../Header';
 import {PagesRouter} from '../../routes/PagesRouter';
 
 export const App = () => {
   return (
-    <BrowserRouter basename="/StarWarsExplorer">
-      <main>
-        <Header />
-        <PagesRouter />
-      </main>
-    </BrowserRouter>
+    <HashRouter>
+      <BrowserRouter basename="/StarWarsExplorer">
+        <main>
+          <Header />
+          <PagesRouter />
+        </main>
+      </BrowserRouter>
+    </HashRouter>
   );
 };
